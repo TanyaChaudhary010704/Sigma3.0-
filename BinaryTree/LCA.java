87a1a496315812f749d63de3c86c28e03db6e43a
TanyaChaudhary010704 <punch010704@gmail.com>
2025-02-22 19:25:02 +0530

    Transform to sum tree

--- BinaryTree/LCA.java
+++ BinaryTree/LCA.java
@@ -1,5 +1,7 @@
 package BinaryTree;
+
 import java.util.ArrayList;
+
 //Lowest common ancestor
 public class LCA {
   static class Node {
@@ -13,9 +15,10 @@ public class LCA {
       this.right = null;
     }
   }
-  
+
   public static boolean getPath(Node root, int node, ArrayList<Node> path) {
-    if(root == null) return false;
+    if (root == null)
+      return false;
     path.add(root);
     if (root.data == node) {
       return true;
@@ -59,15 +62,15 @@ public class LCA {
     return root;
   }
 
-  public static int kthAncestor(Node root, int n,int k) {
+  public static int kthAncestor(Node root, int n, int k) {
     if (root == null) {
       return -1;
     }
     if (root.data == n) {
       return 0;
     }
-    int leftDis = kthAncestor(root.left, n,k);
-    int rightDis = kthAncestor(root.right, n,k);
+    int leftDis = kthAncestor(root.left, n, k);
+    int rightDis = kthAncestor(root.right, n, k);
     if (leftDis == -1 && rightDis == -1) {
       return -1;
     }
@@ -75,9 +78,9 @@ public class LCA {
     if (max + 1 == k) {
       System.out.println(root.data);
     }
-    return max+1;
+    return max + 1;
   }
-  
+
   public static int lcaDis(Node root, int n) {
     if (root == null) {
       return -1;
@@ -90,17 +93,39 @@ public class LCA {
     if (leftDis == -1 && rightDis == -1) {
       return -1;
     } else if (leftDis == -1) {
-      return rightDis+1;
+      return rightDis + 1;
     } else {
-      return leftDis+1;
+      return leftDis + 1;
     }
   }
-  
-  public static int minDistance(Node root,int n1,int n2) {
-    Node lca = lca2(root,n1,n2);
+
+  public static int minDistance(Node root, int n1, int n2) {
+    Node lca = lca2(root, n1, n2);
     int dis1 = lcaDis(lca, n1);
     int dis2 = lcaDis(lca, n2);
     return dis1 + dis2;
+  }
+
+  public static int sumTree(Node root) {
+    if (root == null) {
+      return 0;
+    }
+    int leftSum = sumTree(root.left);
+    int rightSum = sumTree(root.right);
+    int data = root.data;
+    int newLeft = root.left == null ? 0 : root.left.data;
+    int newRight = root.right == null ? 0 : root.right.data;
+    root.data = leftSum + rightSum + newLeft + newRight;
+    return data;
+  }
+  
+  public static void preorder(Node root) {
+    if (root == null) {
+      return;
+    }
+    System.out.println(root.data);
+    preorder(root.left);
+    preorder(root.right);
   }
 
   public static void main(String[] args) {
@@ -114,7 +139,9 @@ public class LCA {
     int n1 = 4, n2 = 5;
     // System.out.println(lca(root, n1, n2).data);
     // System.out.println(lca2(root, n1, n2).data);
-    //System.out.println(minDistance(root, n1, n2));
-    kthAncestor(root, 7, 1);
+    // System.out.println(minDistance(root, n1, n2));
+    // kthAncestor(root, 7, 1);
+    System.out.println(sumTree(root));
+    preorder(root);
   }
 }
