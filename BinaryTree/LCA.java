7fe13e87d3825a33d7c1b463ca92c4c7b4de72d0
TanyaChaudhary010704 <punch010704@gmail.com>
2025-02-22 18:17:08 +0530

    Another method for LCA

--- BinaryTree/LCA.java
+++ BinaryTree/LCA.java
@@ -29,6 +29,7 @@ public class LCA {
     return false;
 
   }
+
   public static Node lca(Node root, int n1, int n2) {
     ArrayList<Node> path1 = new ArrayList<>();
     ArrayList<Node> path2 = new ArrayList<>();
@@ -43,6 +44,21 @@ public class LCA {
     return path1.get(i - 1);
   }
 
+  public static Node lca2(Node root, int n1, int n2) {
+    if (root == null || root.data == n1 || root.data == n2) {
+      return root;
+    }
+    Node leftLCA = lca2(root.left, n1, n2);
+    Node rightLCA = lca2(root.right, n1, n2);
+    if (leftLCA == null) {
+      return rightLCA;
+    }
+    if (rightLCA == null) {
+      return leftLCA;
+    }
+    return root;
+  }
+
   public static void main(String[] args) {
     Node root = new Node(1);
     root.left = new Node(2);
@@ -53,5 +69,6 @@ public class LCA {
     root.right.right = new Node(7);
     int n1 = 4, n2 = 5;
     System.out.println(lca(root, n1, n2).data);
+    System.out.println(lca2(root, n1, n2).data);
   }
 }
